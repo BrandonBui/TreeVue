a1b624bc9c243cee442b76e644f6badcfe8a3594
Ingvar2022 <ingvar20@outlook.com>
2022-10-29 17:47:46 -0700

    Firebase for Android integration complete

--- treevue/lib/main.dart
+++ treevue/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:treevue/SurveyPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {

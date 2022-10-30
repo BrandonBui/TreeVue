8e08c22fb6a51615b2f073553faf43ac8ee940c2
DishaM <disham04@uw.edu>
2022-10-29 22:05:23 -0700

    pages updated

--- treevue/lib/SurveyPageSequence/Page2.dart
+++ treevue/lib/SurveyPageSequence/Page2.dart
@@ -10,14 +10,19 @@ class Page2 extends StatefulWidget {
   State<Page2> createState() => _Page2State();
 }
 
+enum Meats { Chicken, Pork, Beef }
 class _Page2State extends State<Page2> {
+  
   @override
   Widget build(BuildContext context) {
+  Meats? _type = Meats.Chicken;
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 125, 154, 113),
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 57, 74, 47),
-        shadowColor: Colors.white,
         title: Text(
           "Daily Survey",
           textAlign: TextAlign.center,
@@ -26,7 +31,45 @@ class _Page2State extends State<Page2> {
         ),
         toolbarHeight: MediaQuery.of(context).size.height / 4,
       ),
-      body: Column(children: []),
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: EdgeInsets.only(left: width/10, top: height/15),
+          child: Text(
+            "If yes, what kind?",
+            style: GoogleFonts.poppins(fontSize: 25, color: Color.fromARGB(255, 255, 255, 255)),
+            textAlign: TextAlign.left,
+          ),
+        ),
+      Column(
+      children: <Widget>[
+        ListTile(
+          title: const Text('Chicken'),
+          leading: Radio<Meats>(
+            value: Meats.Chicken,
+            groupValue: _type,
+            onChanged: (Meats ? value) {
+              setState(() {
+                _type = value;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: const Text('Beef'),
+          leading: Radio<Meats>(
+            value: Meats.Beef,
+            groupValue: _type,
+            onChanged: (Meats? value) {
+              setState(() {
+                _type = value;
+              });
+            },
+          ),
+        ),
+      ],
+    )
+      ]
+        ),
     );
   }
 }
